50ff2a847588c4a10e5052dc82e9e710c188e811
John Diginee <Johndbizz@gmail.com>
2023-04-19 16:29:16 +0100

    Add APIRouter class

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from todo import todo_router
 
 app = FastAPI()
 
 @app.get("/")
 async def welcome() -> dict:
     return { "welcome": "Hello World" }
+
+app.include_router(todo_router)
